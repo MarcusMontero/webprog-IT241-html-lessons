8b3f3abbf600a56ec81cdd1bc54fb9eb78d2599b
MarcusMontero <mumontero@student.apc.edu.ph>
2026-01-06 03:40:55 +0800

    Update

--- introduction/js/script.js
+++ introduction/js/script.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
   console.log("Personal profile website loaded");
+
+  // Smooth scrolling for nav links
+  const links = document.querySelectorAll("nav ul li a");
+  links.forEach(link => {
+    link.addEventListener("click", function(e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute("href"));
+      target.scrollIntoView({ behavior: "smooth" });
+    });
+  });
 });
