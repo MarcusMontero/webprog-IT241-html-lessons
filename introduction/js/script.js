f4f277bdc601fe1a7fc9cfd97928cce829e6f083
MarcusMontero <mumontero@student.apc.edu.ph>
2026-01-09 09:08:04 +0800

    Update

--- introduction/js/script.js
+++ introduction/js/script.js
@@ -1,2 +1,16 @@
-// Simple JS file for future enhancements
 console.log("Personal Profile Website Loaded");
+
+// Small fade-in animation on scroll
+const boxes = document.querySelectorAll(".section-box");
+
+window.addEventListener("scroll", () => {
+  boxes.forEach(box => {
+    const position = box.getBoundingClientRect().top;
+    const screenHeight = window.innerHeight;
+
+    if (position < screenHeight - 100) {
+      box.style.opacity = "1";
+      box.style.transform = "translateY(0)";
+    }
+  });
+});
